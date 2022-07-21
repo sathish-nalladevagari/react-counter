06add48d4422a9a2e98bc89a3f15a85988126614
sathish kumar reddy <sathish53870@gmail.com>
2022-07-21 21:48:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React ,{ useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const [count,setCount] = useState(0);
+  document.addEventListener('keydown',function(e){
+    if (e.key === 'Enter'){
+      Incre(); 
+    } 
+  })
+
+  const Incre = () =>{
+    setCount(count+1)
+  }
+  const Decre = () =>{
+    setCount(count-1)
+  }
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Counter App</h1>
+        <div className='main'>
+            <h2>{count}</h2>
+            <button onClick={Incre} type="">+</button>
+            <button onClick={Decre} type="">-</button>
+        </div>
     </div>
   );
 }
