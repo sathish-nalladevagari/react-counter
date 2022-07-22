4385ef4126a2149744bf5db9bab55917cd8d4ccb
Sathish reddy <104757035+sathish-nalladevagari@users.noreply.github.com>
2022-07-22 19:15:08 +0530

    Updates

---
+++ precache-manifest.384b13c0593bd743f1c3eeaaa53aa12c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea77d15d9ef07ebf9857",
+    "url": "/react-counter/static/css/main.a311b95b.chunk.css"
+  },
+  {
+    "revision": "ea77d15d9ef07ebf9857",
+    "url": "/react-counter/static/js/main.ea77d15d.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/react-counter/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "1fb84335d4ce8f26b1b6",
+    "url": "/react-counter/static/js/2.1fb84335.chunk.js"
+  },
+  {
+    "revision": "5cb7474a6d5e3f9c494f",
+    "url": "/react-counter/static/js/runtime~main.5cb7474a.js"
+  },
+  {
+    "revision": "424563f59e31b92865bdbf96f8c906be",
+    "url": "/react-counter/index.html"
+  }
+];
